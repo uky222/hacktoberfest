2fb6ebf6e90a87aeee820281b429cde2a9840b7e
Utkarsh Yadav <72603675+uky222@users.noreply.github.com>
2024-10-13 23:49:05 +0530

    Create Hello_world_Utkarsh_Yadav.java

---
+++ scripts/Hello_world_Utkarsh_Yadav.java
@@ -0,0 +1,13 @@
+/*
+  LANGUAGE: JAVA
+  AUTHOR: Utkarsh Yadav
+  GITHUB: https://github.com/uky222
+*/
+
+public class Chathumina {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+
+}
